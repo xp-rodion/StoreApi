8fa5752cc4a8b4b78ba7fc1d5d21f523e7d53bf8
xp-rodion <only.learning.ariy@gmail.com>
2023-02-12 15:56:27 +0300

    feat: add new model Order and redact django model in admin-panel

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from api.models import Item
+from api.models import Item, Order
 
 
 @admin.register(Item)
 class ItemAdminModel(admin.ModelAdmin):
-    fields = ('name', 'description', 'price')
+    fields = ('name', 'description', 'price', 'order')
+    list_display = ('name', 'price')
 
+
+@admin.register(Order)
+class OrderAdminModel(admin.ModelAdmin):
+    list_display = ('order', 'items', )
+
+    def order(self, obj):
+        return obj.__str__()
+
+    def items(self, obj):
+        return list(obj.get_all_items())
--- api/models.py
+++ api/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    name = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+    order = models.ForeignKey('Order', default=None, blank=True, null=True, on_delete=models.PROTECT, verbose_name='Привязка к заказу')
 
     def __str__(self):
         return self.name
@@ -12,3 +13,19 @@ class Item(models.Model):
     class Meta:
         verbose_name = 'предмет'
         verbose_name_plural = 'Предметы'
+
+
+class Order(models.Model):
+
+    def get_all_items(self):
+        return Item.objects.filter(order_id=self.pk)
+
+    def total_sum_order(self):
+        return sum(item.price for item in self.get_all_items())
+
+    def __str__(self):
+        return f'Order #{self.pk}'
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'Заказы'
