7cb89882708f79c214f1c475243cbf1d74faa315
xp-rodion <only.learning.ariy@gmail.com>
2023-02-12 17:07:51 +0300

    feat: add view for Order and redact urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from api.views import BuyAPIView, item
+from api.views import BuyAPIView, item, OrderAPIView, order
 
 app_name = 'api'
 
 urlpatterns = [
     path('buy/<int:item_id>/', BuyAPIView.as_view(), name='buy'),
-    path('item/<int:item_id>/', item, name='item')
+    path('item/<int:item_id>/', item, name='item'),
+    path('buy_order/<int:order_id>/', OrderAPIView.as_view(), name='buy_order'),
+    path('order/<int:order_id>/', order, name='order'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -3,41 +3,62 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.views import APIView
 
-from api.models import Item
+from api.models import Item, Order
 import stripe
 
 stripe.api_key = settings.STRIPE_SEC_KEY
+PC_KEY = settings.STRIPE_PUB_KEY
 
 
-class BuyAPIView(APIView):
+class BaseAPIView(APIView):
+    model = None
 
-    def get(self, request, item_id):
-        item = Item.objects.get(pk=item_id)
-        if item:
-            session = stripe.checkout.Session.create(
-                line_items=[{
-                    'price_data': {
-                        'currency': 'usd',
-                        'product_data': {
-                            'name': item.name,
-                        },
-                        'unit_amount': int(item.price),
+    @staticmethod
+    def get_session(name, price):
+        session = stripe.checkout.Session.create(
+            line_items=[{
+                'price_data': {
+                    'currency': 'rub',
+                    'product_data': {
+                        'name': name,
                     },
-                    'quantity': 1,
-                }],
-                mode='payment',
-                success_url='http://localhost:8000/',
-                cancel_url='http://localhost:8000/',
-            )
-            return JsonResponse({'session_id': session.id})
-        return JsonResponse({'error': 'This item is not available'})
+                    'unit_amount': int(price) * 100,
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url='http://localhost:8000/',
+            cancel_url='http://localhost:8000/',
+        )
+        return JsonResponse({'session_id': session.id})
+
+
+class BuyAPIView(BaseAPIView):
+    model = Item
+
+    def get(self, request, item_id, model=model):
+        obj = model.objects.get(pk=item_id)
+        return super(BuyAPIView, self).get_session(name=obj.name, price=obj.price)
+
+
+class OrderAPIView(BaseAPIView):
+    model = Order
+
+    def get(self, request, order_id, model=model):
+        obj = model.objects.get(pk=order_id)
+        return super(OrderAPIView, self).get_session(name=f'Order #{obj.pk}', price=obj.total_sum_order())
 
 
 def item(request, item_id):
-    pc_key = settings.STRIPE_PUB_KEY
     item = Item.objects.get(pk=item_id)
-    context = {'title': f'Item: {item.name}', 'item': item, 'pc_key': pc_key}
+    context = {'title': f'Item: {item.name}', 'item': item, 'pc_key': PC_KEY}
     template_name = 'item.html'
     return render(request, template_name, context)
 
+
+def order(request, order_id):
+    order = Order.objects.get(id=order_id)
+    context = {'title': f'Order #{order.id}', 'order': order, 'pc_key': PC_KEY}
+    template_name = 'order.html'
+    return render(request, template_name, context)
 
